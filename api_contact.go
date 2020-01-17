bd250a4e673fa8b2aef1734c02069a6a7291a8b0
soopsio <admin@soops.io>
2020-01-17 16:22:37 +0800

    fix

--- api_contact.go
+++ api_contact.go
@@ -2,7 +2,12 @@ package godingtalk
 
 import (
 	"fmt"
+	"github.com/jinzhu/copier"
+	jsoniter "github.com/json-iterator/go"
+	"math/big"
 	"net/url"
+	"strconv"
+	"strings"
 )
 
 type User struct {
@@ -28,31 +33,119 @@ type User struct {
 
 type UserInfo struct {
 	OAPIResponse
-	UnionId         string      `json:"unionid"`
-	OpenId          string      `json:"openid"`
-	Userid          string      `json:"userid"`
-	Roles           []Role      `json:"roles"`
-	IsLeaderInDepts string      `json:"isLeaderInDepts"`
-	IsBoss          bool        `json:"isBoss"`
-	HiredDate       int64       `json:"hiredDate"`
-	IsSenior        bool        `json:"isSenior"`
-	Department      []int       `json:"department"`
-	OrderInDepts    string      `json:"orderInDepts"`
-	Mobile          string      `json:"mobile"`
-	ErrMsg          string      `json:"errmsg"`
-	Active          bool        `json:"active"`
-	Avatar          string      `json:"avatar"`
-	IsAdmin         bool        `json:"isAdmin"`
-	Tags            interface{} `json:"tags"`
-	IsHide          bool        `json:"isHide"`
-	JobNumber       string      `json:"jobnumber"`
-	Name            string      `json:"name"`
-	StateCode       string      `json:"stateCode"`
-	Position        string      `json:"position"`
-	Email           string      `json:"email"`
-	Remark          string      `json:"remark"`
-	Tel             string      `json:"tel"`
-	WorkPlace       string      `json:"workPlace"`
+	UnionId         string           `json:"unionid"`
+	OpenId          string           `json:"openid"`
+	Userid          string           `json:"userid"`
+	Roles           []Role           `json:"roles"`
+	IsLeaderInDepts LeaderInDepts    `json:"isLeaderInDepts"`
+	IsBoss          bool             `json:"isBoss"`
+	HiredDate       int64            `json:"hiredDate"`
+	IsSenior        bool             `json:"isSenior"`
+	Department      []int            `json:"department"`
+	OrderInDepts    map[int]*big.Int `json:"orderInDepts"`
+	Mobile          string           `json:"mobile"`
+	ErrMsg          string           `json:"errmsg"`
+	Active          bool             `json:"active"`
+	Avatar          string           `json:"avatar"`
+	IsAdmin         bool             `json:"isAdmin"`
+	Tags            interface{}      `json:"tags"`
+	IsHide          bool             `json:"isHide"`
+	JobNumber       string           `json:"jobnumber"`
+	Name            string           `json:"name"`
+	StateCode       string           `json:"stateCode"`
+	Position        string           `json:"position"`
+	Email           string           `json:"email"`
+	Remark          string           `json:"remark"`
+	Tel             string           `json:"tel"`
+	WorkPlace       string           `json:"workPlace"`
+}
+
+type LeaderInDepts map[int]bool
+
+func (u *UserInfo) UnmarshalJSON(b []byte) error {
+	tmpUser := struct {
+		OAPIResponse
+		UnionId         string      `json:"unionid"`
+		OpenId          string      `json:"openid"`
+		Userid          string      `json:"userid"`
+		Roles           []Role      `json:"roles"`
+		IsLeaderInDepts string      `json:"isLeaderInDepts"`
+		IsBoss          bool        `json:"isBoss"`
+		HiredDate       int64       `json:"hiredDate"`
+		IsSenior        bool        `json:"isSenior"`
+		Department      []int       `json:"department"`
+		OrderInDepts    string      `json:"orderInDepts"`
+		Mobile          string      `json:"mobile"`
+		ErrMsg          string      `json:"errmsg"`
+		Active          bool        `json:"active"`
+		Avatar          string      `json:"avatar"`
+		IsAdmin         bool        `json:"isAdmin"`
+		Tags            interface{} `json:"tags"`
+		IsHide          bool        `json:"isHide"`
+		JobNumber       string      `json:"jobnumber"`
+		Name            string      `json:"name"`
+		StateCode       string      `json:"stateCode"`
+		Position        string      `json:"position"`
+		Email           string      `json:"email"`
+		Remark          string      `json:"remark"`
+		Tel             string      `json:"tel"`
+		WorkPlace       string      `json:"workPlace"`
+	}{}
+	err := jsoniter.Unmarshal(b, &tmpUser)
+	if err != nil {
+		return err
+	}
+
+	copier.Copy(u, &tmpUser)
+
+	leaderInDepts := strings.TrimFunc(tmpUser.IsLeaderInDepts, func(r rune) bool {
+		switch r {
+		case rune('{'), rune('}'):
+			return true
+		}
+		return false
+	})
+
+	isLeaderInDepts := LeaderInDepts{}
+	for _, dept := range strings.Split(leaderInDepts, ",") {
+		lead := strings.Split(dept, ":")
+		if len(lead) == 2 {
+			if deptId, err := strconv.Atoi(lead[0]); err == nil {
+				switch lead[1] {
+				case "true":
+					isLeaderInDepts[deptId] = true
+				case "false":
+					isLeaderInDepts[deptId] = false
+				}
+			}
+		}
+	}
+
+	orderInDepts := strings.TrimFunc(tmpUser.OrderInDepts, func(r rune) bool {
+		switch r {
+		case rune('{'), rune('}'):
+			return true
+		}
+		return false
+	})
+
+	deptOrder := map[int]*big.Int{}
+	for _, dept := range strings.Split(orderInDepts, ",") {
+		orders := strings.Split(dept, ":")
+		if len(orders) == 2 {
+			if deptId, err := strconv.Atoi(orders[0]); err == nil {
+				i := big.Int{}
+				if order, ok := i.SetString(orders[1], 10); ok {
+					deptOrder[deptId] = order
+				}
+			}
+		}
+	}
+
+	u.OrderInDepts = deptOrder
+	u.IsLeaderInDepts = isLeaderInDepts
+
+	return nil
 }
 
 type Role struct {
@@ -89,9 +182,16 @@ type DepartmentList struct {
 }
 
 // DepartmentList is 获取部门列表
-func (c *DingTalkClient) DepartmentList() (DepartmentList, error) {
+func (c *DingTalkClient) DepartmentList(id int, fetch_child bool) (DepartmentList, error) {
 	var data DepartmentList
-	err := c.httpRPC("department/list", nil, nil, &data)
+	params := url.Values{}
+	if id == 0 {
+		id = 1
+	}
+	params.Add("id", fmt.Sprintf("%d", id))
+	params.Add("fetch_child", fmt.Sprintf("%t", fetch_child))
+
+	err := c.httpRPC("department/list", params, nil, &data)
 	return data, err
 }
 
